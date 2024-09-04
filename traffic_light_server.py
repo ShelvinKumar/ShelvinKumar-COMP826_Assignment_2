e30ccb228d460c35a1fbd63d9967dc59cd109139
ShelvinKumar <shalvink42@gmail.com>
2024-09-04 18:46:28 +1200

    Save Updated

--- traffic_light_server.py
+++ traffic_light_server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 # Sample data to represent traffic light states
 traffic_lights = {
